c9335d684646bcf92aa1143e3b207243790faff9
Arkadiusz Staśko <Arkadiusz.Stasko@microsoft.wsei.edu.pl>
2022-04-03 17:00:51 +0200

    Add dataAccessFactory services methods

--- TimeManager.DataAccess/DataAccessFactory.cs
+++ TimeManager.DataAccess/DataAccessFactory.cs
@@ -1,5 +1,7 @@
 ﻿using TimeManager.DataAccess.models.activity;
 using TimeManager.DataAccess.models.category;
+using TimeManager.DataAccess.services.activityServices;
+using TimeManager.DataAccess.services.categoryServices;
 
 namespace TimeManager.DataAccess
 {
@@ -7,5 +9,7 @@ namespace TimeManager.DataAccess
     {
         public static IActivity GetActivityInstance() => new Activity();
         public static ICategory GetCategoryInstance() => new Category();
+        public static IActivityServices GetActivityServicesInstance() => new ActivityServices();
+        public static ICategoryServices GetCategoryServicesInstance() => new CategoryServices();
     }
 }
