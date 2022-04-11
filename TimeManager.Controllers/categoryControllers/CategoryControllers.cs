5d06b09cb2003337889e4af0b5785a2dff0e4ca2
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-04-11 21:02:43 +0200

    set db and controllers

--- TimeManager.Controllers/categoryControllers/CategoryControllers.cs
+++ TimeManager.Controllers/categoryControllers/CategoryControllers.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimeManager.DataAccess.models.category;
 
 namespace TimeManager.Controllers.categoryControllers
 {
-    public class CategoryControllers : ICategoryControllers
+    public class CategoryControllers : DbContext, ICategoryControllers
     {
+        public CategoryControllers(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+
     }
 }
--- TimeManager.DataAccess/services/activityServices/ActivityServices.cs
+++ TimeManager.DataAccess/services/activityServices/ActivityServices.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimeManager.DataAccess.models.activity;
 
 namespace TimeManager.DataAccess.services.activityServices
 {
-    public class ActivityServices : IActivityServices
+    public class ActivityServices : DbContext, IActivityServices
     {
+
+        public ActivityServices(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Activity> Activities { get; set; }
     }
 }
