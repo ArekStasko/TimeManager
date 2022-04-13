95a71e29ae473c15964fe9ba3217ed9cd47b070d
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-04-13 22:36:00 +0200

    Refactor project

--- TimeManager.DataAccess/Class1.cs
+++ TimeManager.DataAccess/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TimeManager.DataAccess.services.categoryServices
+namespace TimeManager.DataAccess
 {
-    public interface ICategoryServices
+    public class Class1
     {
     }
 }
