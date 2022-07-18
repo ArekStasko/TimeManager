cd998d890d3d156087063cd53e6cc2df31371d58
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-07-18 17:43:08 +0200

    Add IsAuth method

--- TimeManager.API/Processors/Auth_Processor.cs
+++ TimeManager.API/Processors/Auth_Processor.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TimeManager.API.Data;
 using TimeManager.API.Data.Response;
+using TimeManager.API.Authentication;
+using TimeManager.API.Processors.AuthenticationProcessor;
 
 namespace TimeManager.API.Processors
 {
@@ -11,5 +13,18 @@ namespace TimeManager.API.Processors
         public Auth_Processor(DataContext context) : base(context) { }
 
         
+        public bool IsAuth(Token token)
+        {
+            if(token == null) return false;
+
+            var utilities = new User_Utilities(_context);
+            if (_context.Tokens.Contains(token))
+            {
+                var user = _context.Users.FirstOrDefault(u => u.Id == token.userId);
+                return utilities.IsAuthorised(user);
+            };
+
+            return false;
+        }
     }
 }
